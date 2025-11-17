b7484394b62be00f0431a28396c54db965d7eb41
David Meijer <david-meijer@live.nl>
2025-11-17 13:49:10 +0100

    ADD: dev cache dir path

--- scripts/dev_backend.sh
+++ scripts/dev_backend.sh
@@ -20,6 +20,9 @@ export DB_PASS=apppass_ro
 export REDIS_URL="redis://localhost:6379/0"
 export SESSION_TTL_SECONDS=$((7 * 24 * 3600))
 
+# Define cache dir for backend (temp files, etc.)
+export CACHE_DIR="$(pwd)/cache"
+
 # Make sure Flask can find the app
 export PYTHONPATH="$(pwd)/src/server"
 
